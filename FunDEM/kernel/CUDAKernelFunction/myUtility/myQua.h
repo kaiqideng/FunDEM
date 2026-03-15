8d4cced243541904d73ac170f0155e243f78184d
kaiqideng <dengkaiqi7@gmail.com>
2026-03-15 14:05:09 +0000

    update

--- FunDEM/kernel/CUDAKernelFunction/myUtility/myQua.h
+++ FunDEM/kernel/CUDAKernelFunction/myUtility/myQua.h
@@ -50,7 +50,7 @@ HOST_DEVICE inline quaternion operator/(quaternion q, double c) {
 
 HOST_DEVICE inline quaternion normalize(quaternion q) {
     double lenSq = q.q0 * q.q0 + q.q1 * q.q1 + q.q2 * q.q2 + q.q3 * q.q3;
-    if (lenSq < 1.0e-20) return q; // Prevent NaN
+    if (lenSq < kMinPhysicalValue) return q; // Prevent NaN
 
 #ifdef __CUDA_ARCH__
     double invLen = rsqrt(lenSq); // Use hardware intrinsic on GPU
